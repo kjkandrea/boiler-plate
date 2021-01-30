05418b37c597f03e7c9a4d781ca0056449104883
Jongkeun Kim <kjkandrea@gmail.com>
2021-01-30 22:57:32 +0900

    Connect mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI, {
+	useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+})
+	.then(() => {
+		console.log('MongoDB connected...')
+	})
+	.catch(console.log)
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
