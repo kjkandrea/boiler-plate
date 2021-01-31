5716d62a0e9bf11d744e4dfa3065969e2798e0a8
Jongkeun Kim <kjkandrea@gmail.com>
2021-01-31 15:39:38 +0900

    Add body parser

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 const config = require('./config/key')
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json())
 
 mongoose.connect(config.mongoURI, {
 	useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
