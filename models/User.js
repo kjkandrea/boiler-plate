9399e21322ef3843b6353ff14c9e001be101ff80
Jongkeun Kim <kjkandrea@gmail.com>
2021-01-31 16:15:07 +0900

    Add password encode

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -29,6 +30,23 @@ const userSchema = mongoose.Schema({
 	tokenExp: {
 		type: Number
 	}
+})
+
+userSchema.pre('save', (next) => {
+	const saltRounds = 10
+
+	// password 를 bcrypt 로 암호화 한다.
+	bcrypt.genSalt(saltRounds, (error, salt)  => {
+		if (error) return next(error)
+
+		bcrypt.hash(this.password, salt, (error, hash) => {
+			if (error) return next(error)
+			this.password = hash
+			next()
+		})
+	});
+
+	next()
 })
 
 const User = mongoose.model('User', userSchema)
