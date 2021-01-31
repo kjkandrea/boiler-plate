a2f8f57f421af2b9835fcf509bf656332f32726e
Jongkeun Kim <kjkandrea@gmail.com>
2021-01-31 15:39:50 +0900

    Add user save

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => {
 	// 회원가입 할때 필요한 정보들을 client 에서 가져오면 데이터 베이스에 넣어준다.
+
+	const user = new User(req.body)
+
+	user.save((error, userInfo) => {
+		if(error) return res.json({ success: false, error})
+		return res.status(200).json ({
+			success: true
+		})
+	})
 })
 
 app.listen(port, () => {
