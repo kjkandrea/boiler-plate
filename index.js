4cc77746f5b16f6908350a94324fd9ba46e6b5a7
kjkandrea <kjkandrea@gmail.com>
2021-02-02 20:20:09 +0900

    Add logout

--- index.js
+++ index.js
@@ -85,6 +85,15 @@ app.get('/api/users/auth', auth, (req, res) => {
 	})
 })
 
+app.get('/api/users/logout', auth, (req, res) => {
+	User.findOneAndUpdate({ _id: req.user._id }, {token: ""}, (error) => {
+		if (error) return res.json({ success: false, error})
+		return res.status(200).send({
+			success: true
+		})
+	})
+})
+
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
